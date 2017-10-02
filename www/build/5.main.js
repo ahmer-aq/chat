38f8280981eb31480fcf7bc7813ea03e33ecd489
ahmer-aq <ahmer_aq@yahoo.com>
2017-10-02 17:29:42 +0500

    add www and proc

---
+++ www/build/5.main.js
@@ -0,0 +1,45 @@
+webpackJsonp([5],{
+
+/***/ 297:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(10);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__messages_list__ = __webpack_require__(111);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MessagesListPageModule", function() { return MessagesListPageModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var MessagesListPageModule = (function () {
+    function MessagesListPageModule() {
+    }
+    return MessagesListPageModule;
+}());
+MessagesListPageModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__messages_list__["a" /* MessagesListPage */],
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__messages_list__["a" /* MessagesListPage */]),
+        ],
+        exports: [
+            __WEBPACK_IMPORTED_MODULE_2__messages_list__["a" /* MessagesListPage */]
+        ]
+    })
+], MessagesListPageModule);
+
+//# sourceMappingURL=messages-list.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=5.main.js.map
